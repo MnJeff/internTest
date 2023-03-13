945cccc70a9c8572ada8adc2e9646bc2a1bcdcdd
Your Name <tuanjoker1971@gmail.com>
2023-03-13 15:00:33 +0700

    a

--- App.js
+++ App.js
@@ -22,11 +22,7 @@ const db = firebase.firestore();
 const jokes = [
   {
     id: 1,
-    text: `A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on."
-
-    The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."
-    
-    The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."`,
+    text: `A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on."The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."`,
     funny: 0,
     notfunny: 0,
   },
